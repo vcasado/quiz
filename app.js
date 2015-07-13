abf94296f2be26635c1277922ba41cfd88c693ab
Victor Casado <victorct83@gmail.com>
2015-07-13 20:41:53 +0200

    añadida página de créditos

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(partials());
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use('/public', express.static(__dirname + '/public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET credits page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
 module.exports = router;
