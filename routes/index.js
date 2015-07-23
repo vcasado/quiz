282b356d5e93a74a41281b16e2f0a32cf8608784
Victor Casado <victorct83@gmail.com>
2015-07-23 18:13:57 +0200

    añadidas bases de datos

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);	// autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes',	quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /* GET credits page. */
 router.get('/author', function(req, res) {
